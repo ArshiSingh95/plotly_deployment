a31222086c0aef5b4db9632ccb7863c1236c0e86
Arshi Singh <sarsdip@gmail.com>
2020-12-19 19:06:04 -0800

    challenge

--- js/plot.js
+++ js/plot.js
@@ -14,6 +14,19 @@ function init() {
 
 init();
 
-// function optionChanged(newSample) {
-//   console.log(newSample);
-// }
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("js/samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.location);
+  });
+}
